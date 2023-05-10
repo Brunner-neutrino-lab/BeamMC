61cb4f5a07a43078af10785e9c7ed95c26994d37
lucas darroch <darroch.lucas@gmail.com>
2023-05-10 15:10:12 -0400

    updated scipt with extra beam shape. Re organized folder structure. Script will setup folders for each beam shape, and indexes the data better.

--- beamMC.py
+++ beamMC.py
@@ -8,17 +8,36 @@ Created on Sat Oct 29 01:10:22 2022
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 import time
 import os
 
 # Create an array of photons with a gaussian beam profile
-def MCbeam():
+def MCbeam(n):
     X=[];Y=[]
-    for i in range(N):
+    for i in range(n):
         X.append(random.gauss(0,w1/2))
         Y.append(random.gauss(0,w2/2))
     data['hitx'] = X
     data['hity'] = Y
+
+# Create an array of photons with one-dimension uniform and the other gaussian
+def MCslit(n):
+    X=[];Y=[]
+    for i in range(n):
+        X.append(random.gauss(0,w1/2))
+        Y.append(random.uniform(-dy/2,dy/2))
+    data['hitx'] = X
+    data['hity'] = Y    
+
+# Create an array of photons with a uniform beam profile
+def MCuni(n):
+    X=[];Y=[]
+    for i in range(n):
+        X.append(random.uniform(-dx/2,dx/2))
+        Y.append(random.uniform(-dy/2,dy/2))
+    data['hitx'] = X
+    data['hity'] = Y    
 
 # Define the bounds for the indexed SPAD
 def cell(i,ds):
@@ -40,7 +59,7 @@ def raster(t0,xs,xe,ys,ye):
                 continue  
             y1,y2 = cell(j,dy)   
             # Initialize number of hits and the hit array for the indexed SPAD
-            n=0;rm=[]
+            nhit=0;rm=[]
             # Iterate through photon array, checking if they hit the indexed SPAD
             for k in range(len(data['hitx'])):
                 if x1 < data['hitx'][k]:
@@ -48,9 +67,10 @@ def raster(t0,xs,xe,ys,ye):
                         if y1 < data['hity'][k]:
                             if y2 > data['hity'][k]:
                                 # If the photon hits the indexed SPAD, count it and add the photon index to the hit array
-                                n+=1;rm.append([data['hitx'][k],data['hity'][k]])
+                                nhit+=1;rm.append([data['hitx'][k],data['hity'][k]])
             # Count the total number of hits for the indexed SPAD
-            data['x%i_y%i'%(i,j)] = n
+            data['x%i_y%i'%(i,j)] = nhit
+            data['hits'].append(nhit)
             # Remove photons that have been counted from the photon array
             for ph in rm:
                 data['hitx'].remove(ph[0])
@@ -72,16 +92,19 @@ def MCSiPM(pde):
     # Compare the physical coordinates against the beam radius
     # Determine the SPAD indicies within each box of radius i*w0/2
     rx=[];ry=[]
-    for i in range(1,5):
-        rx.append([np.where(x<-w1*i/2)[0][-1],np.where(x<w1*i/2)[0][-1]])
-        ry.append([np.where(y<-w2*i/2)[0][-1],np.where(y<w2*i/2)[0][-1]])
+    if w1 < dx and w2 < dy:
+        for i in range(1,5):
+            rx.append([np.where(x<-w1*i/2)[0][-1],np.where(x<w1*i/2)[0][-1]])
+            ry.append([np.where(y<-w2*i/2)[0][-1],np.where(y<w2*i/2)[0][-1]])
     rx.append([0,xi]);ry.append([0,yi])
     # Array for tracking which SPAD has been checked
     data['sipm'] = np.ones([xi,yi])
+    # Flat array for tracking the number of hits
+    data['hits'] = []
     # Raster boxes with increasing size about the center of the beam - this increases the speed of the process
     for i in range(len(rx)):
         raster(T0,rx[i][0],rx[i][1],ry[i][0],ry[i][1]) 
-    # Simulate firing SPADs
+    # Flat array for tracking SPADs that should avalanche
     data['ava'] = []
     for i in range(xi):
         for j in range(yi):
@@ -92,6 +115,7 @@ def MCSiPM(pde):
             data['ava'].append(data['x%i_y%i_ava'%(i,j)])
 
 def SPADinit():
+    # Loop through the SPAD indicies
     for i in range(xi):
         for j in range(yi):     
             # Initialize the SPAD hit array
@@ -100,8 +124,10 @@ def SPADinit():
 # Determine the SPAD-hit distribution
 def MChit():
     Nhit = 0;Nmult = 0;N1 = 0;N0 = 0
+    # Loop through the SPAD indicies
     for i in range(xi):
         for j in range(yi):
+            # Count the number of hits, multiple hits, and single hits
             Nhit += data['x%i_y%i'%(i,j)]
             if data['x%i_y%i'%(i,j)] == 0:
                 N0 += 1
@@ -110,7 +136,9 @@ def MChit():
             elif data['x%i_y%i'%(i,j)] > 1:
                 Nmult += 1
             else:
+                # If the number of hits is not an expected result, print the error
                 print('except %s'%data['x%i_y%i'%(i,j)])
+    # Return the number of hits, multiple hits, and single hits
     return Nhit,Nmult,N1,N0
 
 # Determine the SPAD-detection distribution
@@ -126,74 +154,86 @@ def MCdetect():
                 Nmiss += data['x%i_y%i_ava'%(i,j)]-1
     return Nhit,Nmiss,N1,N0
 
-def updatesim(i,t0,pde,sim,z):
-    if i == 0:
-        if z == 0:
+def updatesim(s,t0,pde,sim,p):
+    if s == 0:
+        if p == 0:
             print('Simulation beginning\n')
-    elif i % 10 == 0:
-        print('Status: PDE = %.2f %%'%(pde*100))
+    # Print the percentage of simulation processed, updating every 10%
+    elif s % (sim/10) == 0:
+    #elif s % 10 == 0:
+        print('Status: PDE = %.2f %% - %i %% complete'%(pde*100,s/sim*100))
         # Check how much time is remaining
-        pc_part = sim/(i)
+        pc_part = sim/(s)
         t=time.time()-t0
         tf = t*(pc_part-1)/60
         print('%i minutes remaining on this PDE'%(tf))
-        wh_part = PDEs/(z+1/pc_part)
+        wh_part = PDEs/(p+1/pc_part)
         t=time.time()-start
         tf = t*(wh_part-1)/60
         print('%i minutes remaining on the simulation\n'%(tf))
 
 # Guassian beam plot
-def plotflux(i):
-    plots['fig_flux'],plots['ax_flux']=plt.subplots(1,1,dpi=100)
-    plots['ax_flux'].hist2d(data['hitx'],data['hity'],bins=(xi,yi),range=[[-dx/2,dx/2],[-dx/2,dx/2]])
-    plots['ax_flux'].set_xlabel('X position [mm]')
-    plots['ax_flux'].set_ylabel('Y position [mm]')
-    plots['ax_flux'].axis('square')
-    plots['ax_flux'].axis('square')
-    plots['fig_flux'].savefig('plots/simulation_%i.png'%i)
-    plots['fig_flux1d'],plots['ax_flux1d']=plt.subplots(2,1,dpi=100)
-    plots['ax_flux1d'][0].hist(data['hitx'],bins=xi)
-    plots['ax_flux1d'][1].hist(data['hity'],bins=yi)
-    plots['ax_flux1d'][0].set_xlabel('X position [mm]')
-    plots['ax_flux1d'][1].set_xlabel('Y position [mm]')
-    plots['ax_flux1d'][0].set_ylabel('SPAD hits')
-    plots['ax_flux1d'][1].set_ylabel('SPAD hits')
-    plots['fig_flux1d'].savefig('plots/simulation1d_%i.png'%i)
-    plt.close()
+def plotflux(s,n):
+    if s == 0:
+        # Plot the flux
+        plots['fig_flux'],plots['ax_flux']=plt.subplots(1,1,dpi=100)
+        plots['ax_flux'].hist2d(data['hitx'],data['hity'],bins=(xi,yi),range=[[-dx/2,dx/2],[-dx/2,dx/2]])
+        #plots['ax_flux'].colorbar()
+        plots['ax_flux'].set_xlabel('X position [mm]')
+        plots['ax_flux'].set_ylabel('Y position [mm]')
+        plots['ax_flux'].axis('square')
+        plots['ax_flux'].axis('square')
+        plt.tight_layout()
+        plots['fig_flux'].savefig('plots/%s/sim-flux_%i_Nph_%i.png'%(SIM,s,n))
+        plt.close(plots['fig_flux'])
+        # Plot the flux in 1D
+        plots['fig_flux1d'],plots['ax_flux1d']=plt.subplots(2,1,dpi=100)
+        plots['ax_flux1d'][0].hist(data['hitx'],bins=xi,range=[-dx/2,dx/2])
+        plots['ax_flux1d'][1].hist(data['hity'],bins=yi,range=[-dy/2,dy/2])
+        plots['ax_flux1d'][0].set_xlabel('X position [mm]')
+        plots['ax_flux1d'][1].set_xlabel('Y position [mm]')
+        plots['ax_flux1d'][0].set_ylabel('SPAD hits')
+        plots['ax_flux1d'][1].set_ylabel('SPAD hits')
+        plt.tight_layout()
+        plots['fig_flux1d'].savefig('plots/%s/sim-flux1d_%i_Nph_%i.png'%(SIM,s,n))
+        #plt.close(plots['fig_flux1d'])
 
 # SPAD-hit distribution    
-def plothits():
-    hits=[]
-    for i in range(xi):
-        for j in range(yi):
-            hits.append(data['x%i_y%i'%(i,j)])
-    plots['fig_hits'],plots['ax_hits']=plt.subplots(1,1,dpi=100)
-    plots['ax_hits'].hist(hits,bins=np.arange(20))
-    plots['ax_hits'].set_xlabel('Number of photons hitting a single SPAD')
-    plots['ax_hits'].set_ylabel('SPAD hit frequency')
-    plots['ax_hits'].set_yscale('log')
-    plt.close()
+def plothits(s,n):
+    if s == 0:
+        # Plot the SPAD-hit distribution
+        plots['fig_hits'],plots['ax_hits']=plt.subplots(1,1,dpi=100)
+        plots['ax_hits'].hist(data['hits'],bins=np.arange(int(max(data['hits']))+2),align='left')
+        plots['ax_hits'].set_xlabel('Number of photons hitting a single SPAD')
+        plots['ax_hits'].set_ylabel('SPAD hit frequency')
+        plots['ax_hits'].set_yscale('log')
+        plots['ax_hits'].xaxis.set_major_locator(MaxNLocator(integer=True))
+        plt.tight_layout()
+        plots['fig_hits'].savefig('plots/%s/sim-hit-density_%i_Nph_%i.png'%(SIM,s,n))
+        plt.close(plots['fig_hits'])
 
-# SPAD-hit distribution    
-def plotava():
-    hits=[]
-    for i in range(xi):
-        for j in range(yi):
-            hits.append(data['x%i_y%i_ava'%(i,j)])
-    plots['fig_ava'],plots['ax_ava']=plt.subplots(1,1,dpi=100)
-    plots['ax_ava'].hist(hits,bins=np.arange(10))
-    plots['ax_ava'].set_xlabel('Number of photon-induced avalanches')
-    plots['ax_ava'].set_ylabel('SPAD avalanche frequency')
-    plots['ax_ava'].set_yscale('log')
-    plt.close()
+# SPAD-avalanche distribution    
+def plotava(s,n):
+    if s == 0:
+        # Plot the SPAD-avalanche distribution
+        plots['fig_ava'],plots['ax_ava']=plt.subplots(1,1,dpi=100)
+        plots['ax_ava'].hist(data['ava'],bins=np.arange(int(max(data['ava']))+2),align='left')
+        plots['ax_ava'].set_xlabel('Number of photon-induced avalanches')
+        plots['ax_ava'].set_ylabel('SPAD avalanche frequency')
+        plots['ax_ava'].set_yscale('log')
+        plots['ax_ava'].xaxis.set_major_locator(MaxNLocator(integer=True))
+        plt.tight_layout()
+        plots['fig_ava'].savefig('plots/%s/sim-avalanche-density_%i_Nph_%i.png'%(SIM,s,n))
+        plt.close(plots['fig_ava'])
     
 # Measured and missing avalanches
-def plotresults(pde):
-    mc=np.average(data['mc']);mcs=np.std(data['mc'])/np.sqrt(len(data['mc']))
-    xc=np.average(data['xc']);xcs=np.std(data['xc'])/np.sqrt(len(data['xc']))
+def plotresults(pde,n):
+    mc=np.average(data['mc']);mcs=np.std(data['mc'])/np.sqrt(len(data['mc'])-1)
+    xc=np.average(data['xc']);xcs=np.std(data['xc'])/np.sqrt(len(data['xc'])-1)
     fig,ax=plt.subplots(2,1,dpi=100,figsize=(8,8))
-    ax[0].hist(data['mc'],label='%.2f +/- %.2f'%(mc,mcs))         
-    ax[1].hist(data['xc'],label='%.2f +/- %.2f'%(xc,xcs))
+    param_str = "PDE = %.2f %%\nN = %i\nN_cells = %i\nSimulation = %s\n"%(pde*100,n,Ncells,SIM)
+    ax[0].hist(data['mc'],label=param_str+'%.2f +/- %.2f'%(mc,mcs))         
+    ax[1].hist(data['xc'],label=param_str+'%.2f +/- %.2f'%(xc,xcs))
     ax[0].set_xlabel('Measured avalanches')
     ax[0].set_ylabel('MC frequency')
     ax[1].set_xlabel('Missing avalanches')
@@ -201,69 +241,91 @@ def plotresults(pde):
     ax[0].legend()
     ax[1].legend()
     plt.tight_layout()
-    fig.savefig('plots/results_PDE%i.png'%(100*pde))
-    plt.close()
+    fig.savefig('plots/%s/results_PDE_%i_Nph_%i_Ncells_%i.png'%(SIM,100*pde,n,Ncells))
+    plt.close(fig)
    
-def main(pde,z):   
+def main(pde,p,n):   
     # Main function
     # Repeat measurement
     data['mc'],data['xc']=[],[]
-    sims=10;t0=time.time()
-    for i in range(sims):
-        updatesim(i,t0,pde,sims,z)
-        # MC Gaussian beam
-        MCbeam()
-        # Plot the Gaussian beam
-        #plotflux(i)  
+    sims=100;t0=time.time()
+    for s in range(sims):
+        updatesim(s,t0,pde,sims,p)
+        # MC beam
+        if SIM == 'gauss':
+            MCbeam(n)
+        if SIM == 'slit':
+            MCslit(n)
+        if SIM == 'uni':
+            MCuni(n)
+        # Plot the beam
+        plotflux(s,n)  
         # Determine how many photons hit each SPAD
         MCSiPM(pde) 
         # Determine the SPAD-hit distribution
         Hhit,Hmiss,H1,H0=MChit()
         # Plot a histogram of the SPAD-hit distribution
-        #plothits()
+        plothits(s,n)
         # Determine how many SPADs fire
         Ahit,Amiss,A1,A0=MCdetect()
         # Plot a histogram of the SPAD-avalanche distribution
-        #plotava()    
+        plotava(s,n)    
         data['mc'].append(Ahit)
         data['xc'].append(Amiss)
     # Plots the results of the simulation for the current PDE    
-    plotresults(pde)
+    plotresults(pde,n)
+    # Save the data
+
     
 if __name__ == "__main__":  
+    # Start the timer
     start=time.time()
     
     # Magic numbers 
     # Beam profile       
-    # How many photons are in the pulse     
-    N = int(6.761728E6/500)
+    # How many photons are in the pulse    
+    Nmin = 0;Nmax = 4
+    num = Nmax-Nmin+1 
+    N = np.logspace(Nmin,Nmax,num)
+    N = [1,2,4,8,10,20,40,80,100,200,400,800,1000,2000,4000,8000,10000,20000,40000,80000,100000]
     # What is the beam radius (Gaussian beam), (x,y)
-    w1=1.29;w2=1.31#mm
+    w1=1;w2=w1 #mm
     # SiPM/SPAD parameters
-    PDE = np.arange(1,30)/100
+    PDE = [0.25]
     PDEs = len(PDE)
     pitch = 50E-3#mm
     # SiPM width/height
     dx = 5.95
     dy = 5.85
+    # What simulation are we running?
+    #SIM = 'gauss'
+    SIM = 'slit'
+    #SIM = 'uni'
     
     # SPAD array
-    xi=int(dx/pitch)
-    yi=int(dy/pitch)
+    xi=round(dx/pitch)
+    yi=round(dy/pitch)
+    # Number of SPADs
+    Ncells=int(xi*yi)
       
     # Make dictionaries
     plots = {}
     data = {}
     # Make directories
-    if not os.path.isdir('plots'):
-        os.mkdir('plots')
+    if not os.path.isdir('plots/%s'%SIM):
+        os.mkdir('plots/%s'%SIM)
     
     # Main function
-    for i in range(PDEs):
-        main(PDE[i],i)
+    for p in range(PDEs):
+        for n in N:
+            print('Throwing %i photons at a PDE of %.2f %%'%(n,PDE[p]*100))
+            main(PDE[p],p,int(n))
  
+    # End the timer
     end=time.time()
-    print('Simulation complete - time elapsed: %.2f hours'%(end-start)/3600)
+
+    # Print the time elapsed
+    print('Simulation complete - time elapsed: %.2f hours'%((end-start)/3600))
 
 
 
---
+++ plot-results.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 29 01:10:22 2022
+
+@author: darro
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import optimize
+from scipy import integrate
+from scipy import special
+
+def saturation(x, x0, k):
+    return x0 * (1 - np.exp(-k * x / x0))
+
+def x0(r):
+    a = 50E-3
+    return np.pi*r**2/a
+
+def x0_(r):
+    a = (50E-3)**2
+    return 2*np.pi*r/a
+
+def n0(r,n):
+    s = 0.5
+    return -n*np.exp(-r**2/2/s**2)
+
+def n0_(r,n):
+    s = 0.5
+    return n*r/s**2*np.exp(-r**2/2/s**2)
+
+def Nfired_(r,n):
+    k = 0.25
+    return x0_(r)*(1-np.exp(-k*n0_(r,n)/x0_(r)))
+
+def Nfired_func(n):
+    k = 0.25
+    a = (50E-3)**2
+    s = 0.5
+    A = (2*np.pi*s**2/a)
+    return A*(np.euler_gamma+np.log(k*n/A)-special.expi(-k*n/A))
+
+Ncells = 100000
+PDE = 0.25
+N = np.array([1,2,4,8,10,20,40,80,100,200,400,800,1000,2000,4000,8000,10000,20000,40000,80000,100000])
+Ngauss = np.array([0.28,0.49,0.96,1.98,2.42,5.24,9.69,20.04,25.11,49.41,95.22,182.81,228.04,417.96,705.60,1096.45,1230.72,1662.68,2099.87,2538.43,2681.9])
+
+#Ngauss = np.array([0.24,0.48,0.99,2.07,2.46,4.88,9.90,19.98,24.63,48.91,96.48,184.82,227.03,415.99,709.06,1096.01,1235.62,1666.84,2102.63,2541.36])
+#Nuni = np.array([0.28,0.51,0.94,2.06,2.44,5.21,10.13,20.33,25.16,49.25,99.19,201.09,249.45,500.31,986.42,1951.99,2426.26,4703.52,8843.50,15728.76])
+
+# Define the figure and axes
+fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+# Plot the data
+ax.plot(N[:len(Ngauss)], Ngauss, 'o')
+#ax.plot(N[:len(Nuni)], Nuni, 's')
+# Plot the saturation curve
+x = np.linspace(1, 1E5, int(1E4))
+y = saturation(x, Ncells, PDE)
+ax.plot(x, y, 'r-')
+ax.plot(x, x*PDE, 'k--')
+ax.plot(x, Nfired_func(x), '-')
+
+#ax.plot(x, Nfired_func(x), 'g-')
+# Set the x and y axes labels
+ax.set_xlabel('Number of photons')
+ax.set_ylabel('Number of fired cells')
+# Set the title
+ax.set_title('SiPM saturation curve')
+# Set the x and y axis to be logarithmic
+ax.set_xscale('log')
+ax.set_yscale('log')
+# Show the plot
+plt.show()
+
+# Fit the data to the saturation curve
+#popt, pcov = optimize.curve_fit(saturation, N[:len(Ngauss)], Ngauss)
+# Define the figure and axes
+fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+# Plot the saturation curve
+r = np.linspace(0,10,num=1000)
+for n in N:
+    ax.plot(r, Nfired_(r,n), 'k-')
+# Set the x and y axes labels
+ax.set_xlabel('Radius (mm)')
+ax.set_ylabel('Cell firing gradient (1/mm)')
+# Set the title
+ax.set_title('SiPM Gaussian beam saturation curve')
+# Set the x and y scale
+ax.set_xlim(0, 3)
+ax.set_ylim(0.1, 1E4)
+# Set the x and y axis to be logarithmic
+#ax.set_xscale('log')
+ax.set_yscale('log')
+# Show the plot
+plt.show()
+
+
+'''
+# Define the figure and axes
+fig, ax = plt.subplots(1, 1, dpi=200)
+def sat_function(cr,t):
+    return cr*t/(1+cr*t)*100
+# Plot the sat function
+cr = np.logspace(0,6,num=1000)
+tmin = -7;tmax = -5
+num = tmax-tmin+1 
+t = [0.001,0.01,0.1,1,10,100,1000]
+for dt in t:
+    ax.plot(cr, sat_function(cr,dt*1E-6), '-',label=r'$\Delta t$ = '+str(dt)+r' $\mu s$')
+# Set the font size for the axis labels and axis title
+ax.tick_params(labelsize=14)
+# Set the font size for the x and y labels
+ax.xaxis.label.set_size(14)
+ax.yaxis.label.set_size(14)
+# Set the x and y axes labels
+ax.set_xlabel('Source activity [Bq]')
+ax.set_ylabel('Percent change in\ndetected avalanches (%)')
+# Set the scale to be logarithmic
+ax.set_xscale('log')
+ax.set_yscale('log')
+# Show a grid   
+ax.grid(True, which="both", ls="-", color='0.65')
+# Show the plot
+plt.legend()
+plt.tight_layout()
+plt.show()
+'''
